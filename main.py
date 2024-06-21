cdd1d17037c8aecd19f07ba03f0593582a33ec44
Atharv varshney <Atharvvarshney07@gmail.com>
2024-06-22 04:00:12 +0530

    creating basic structure

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ init(app)
 
 @app.get("/")
 def home():
-    names=["aludayalu", "potato"]
-    cards=[]
-    for name in names:
-        cards.append(render("components/card.html", locals()))
+    header = render("components/header.html", locals())
+    footer = render("components/footer.html", locals())
     return render("components/index.html", locals())
-
 
 
 app.run(host="0.0.0.0", port=int(sys.argv[1]))
