d5b62b65e8db8231e29b8631dbb74a913a1205cf
aludayalu <pl46u3.ducklinz@gmail.com>
2024-06-22 06:44:35 +0530

    database added

---
+++ database.py
@@ -0,0 +1,13 @@
+from variables import database_uri
+from urllib.parse import quote
+import json, requests
+
+def get(table, key):
+    out=requests.get(database_uri+"/get?key="+quote(table+"/"+key)).text
+    response=json.loads(out)
+    if response["Ok"]:
+        response["Value"]=json.loads(response["Value"])
+    return response
+
+def set(table, key, val):
+    requests.get(database_uri+"/set?key="+quote(table+"/"+key)+"&val="+quote(json.dumps(val)))
