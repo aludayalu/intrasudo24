aca12ead4224886eb5aca6fc927c9b90d1094ffb
aludayalu <pl46u3.ducklinz@gmail.com>
2024-06-22 11:28:43 +0530

    basic auth added

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
 import uuid, json, flask, time, sys
-from flask import Flask, request
+from flask import Flask, request, redirect
 from monster import render, init
 from database import get, set
 from mailer import mail
+import re, hashlib
 
 app=Flask(__name__)
 init(app)
 
+def is_valid_email(email):
+    pattern = r'^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@dpsrkp\.net$'
+    return re.match(pattern, email) is not None
+
 def User():
-    return {"id":0, "name":"", "level":0, "logs":"", "email":"", "verified":False, "password":""}
+    return {"name":"", "level":0, "logs":"", "email":"", "verified":False, "password":""}
 
 def auth(cookies):
-    if "loggedIn" in cookies:
-        return get("tokens", cookies["loggedIn"])
+    if "email" in cookies:
+        return get("emails", cookies["email"])
     else:
         return {"Ok":False}
 
 @app.get("/")
 def home():
     loggedIn=auth(dict(request.cookies))
+    if loggedIn["Ok"]:
+        status="Logout"
+        status_url="/logout"
+    else:
+        status="Log In"
+        status_url="/auth"
     header = render("components/header.html", locals())
     footer = render("components/footer.html", locals())
     confetti = render("components/confetti.html", locals())
     return render("components/index.html", locals())
+
+@app.get("/logout")
+def logout():
+    response=redirect("/")
+    response.set_cookie("email", "")
+    response.set_cookie("password", "")
+    return response
 
 @app.get("/auth")
 def auth_page():
@@ -33,6 +51,28 @@ def auth_page():
 def otp():
     header = render("components/header.html", locals())
     return render("components/otp.html", locals())
+
+@app.get("/api/auth")
+def auth_api():
+    args=dict(request.args)
+    if "password" not in args or "email" not in args or "name" not in args:
+        return json.dumps({"error":"Missing Fields"})
+    if is_valid_email(args["email"]):
+        if get("emails", args["email"])["Ok"]:
+            account=get("accounts", args["email"])["Value"]
+            if account["password"]==hashlib.sha256(args["password"].encode()).hexdigest():
+                return json.dumps({"success":True})
+            else:
+                return json.dumps({"error":"Incorrect Password"})
+        else:
+            set("emails", args["email"], str(time.time()))
+            user=User()
+            user["email"]=args["email"]
+            user["name"]=args["name"]
+            user["password"]=hashlib.sha256(args["password"].encode()).hexdigest()
+            set("accounts", args["email"], user)
+            return json.dumps({"success":True})
+    return json.dumps({"error":"Invalid Email"})
 
 
 app.run(host="0.0.0.0", port=int(sys.argv[1]))
--- public/login.js
+++ public/login.js
@@ -5,7 +5,12 @@ const submitBtn = document.getElementById("submit")
 
 const authTxt = document.getElementById("authTxt")
 
-const signal = Signal("authstate", "signup")
+const signal = Signal("authstate", "login")
+
+function isValidEmail(email) {
+    const pattern = /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@dpsrkp\.net$/;
+    return pattern.test(email);
+}
 
 signal.onChange = () => {
     const temp = authTxt.innerHTML;
@@ -29,5 +34,72 @@ signal.onChange = () => {
 }
 
 signupBtn.addEventListener("click", () => {
-    signal.setValue(signal.Value === "signup" ? "signin" : "signup")
+    signal.setValue(signal.Value() === "signup" ? "login" : "signup")
+})
+
+var notyf = new Notyf();
+var position={x:"center", y:"top"}
+
+function cookie_get(key) {
+    try {
+        var cookies={}
+        for (var x in document.cookie.split("; ")) {
+            var raw_data=document.cookie.split("; ")[x].split("=")
+            cookies[raw_data[0]]=raw_data[1]
+        }
+        if (key in cookies) {
+            return cookies[key]
+        }
+        return ""
+    } catch {
+        return ""
+    }
+}
+
+function cookie_set(key,val) {
+    try {
+        document.cookie=`${key}=${val};expires=Thu, 01 Jan 2049 00:00:00 UTC`
+    } catch {}
+}
+
+submitBtn.addEventListener("click", ()=>{
+    var name=document.getElementById("name").value.trim()
+    var email=document.getElementById("email").value.trim()
+    var password=document.getElementById("password").value.trim()
+    if (signal.Value()=="signup") {
+        if (name.length==0) {
+            notyf.error({position: position, message:"Name field is required"})
+            return
+        }
+    }
+    if (email.length==0) {
+        notyf.error({position: position, message:"Email field is required"})
+        return
+    }
+    if (!isValidEmail(email)) {
+        notyf.error({position: position, message:"Kindly login with valid dpsrkp.net accounts"})
+        return
+    }
+    if (password.length==0) {
+        notyf.error({position: position, message:"Password field is required"})
+        return
+    }
+    fetch("/api/auth?name="+encodeURIComponent(name)+"&email="+encodeURIComponent(email)+"&password="+encodeURIComponent(password)).then(async (x)=>{
+        var out=await x.text()
+        try {
+            var json=JSON.parse(out)
+        } catch {
+            var json=false
+        }
+        if (json!=false && json.success!==undefined) {
+            notyf.success({position: position, message:"Successfully Authenticated"})
+            setTimeout(()=>{
+                cookie_set("email", email)
+                cookie_set("password", password)
+                window.location="/"
+            }, 1000)
+        } else {
+            notyf.error({position: position, message:"Authentication Failed: "+out})
+        }
+    })
 })
