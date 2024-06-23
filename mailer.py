470f2053f3ea9bd4915d0aa6813f61c1382b3993
aludayalu <pl46u3.ducklinz@gmail.com>
2024-06-23 11:14:28 +0530

    porting mail to smtp

--- mailer.py
+++ mailer.py
@@ -1,15 +1,21 @@
-import resend
 from secrets_parser import parse
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib, ssl
 
-resend.api_key = parse("variables.txt")["resend_key"]
-
+password=parse("variables.txt")["password"]
 
 def mail(to, subject, content):
-    params = {
-        "from": "Exun Clan <exun@exun.co>",
-        "to": [to],
-        "subject": subject,
-        "html": content,
-        "reply_to": "exun@dpsrkp.net",
-    }
-    resend.Emails.send(params)
+    message=MIMEMultipart("alternative")
+    message["Subject"]=subject
+    message["From"]="Exun Clan <exun@dpsrkp.net>"
+    message["to"]=to
+    
+    text = content
+    message.attach(MIMEText(text, "html"))
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login("exun@dpsrkp.net", password)
+        server.sendmail(
+            "exun@dpsrkp.net", to, message.as_string()
+    )
