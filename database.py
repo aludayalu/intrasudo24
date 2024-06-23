8ae967b94deae7363b1ac43dfab3b42f581dc5be
aludayalu <pl46u3.ducklinz@gmail.com>
2024-06-23 09:22:50 +0530

    get_all added

--- database.py
+++ database.py
@@ -11,5 +11,12 @@ def get(table, key):
         response["Value"]=json.loads(response["Value"])
     return response
 
+def get_All(table):
+    out=requests.get(database_uri+"/get?key="+quote(table+"/")).text
+    response=json.loads(out)
+    if response["Ok"]:
+        response["Value"]=[json.loads(x) for x in response["Value"]]
+    return response
+
 def set(table, key, val):
     requests.get(database_uri+"/set?key="+quote(table+"/"+key)+"&val="+quote(json.dumps(val)))
