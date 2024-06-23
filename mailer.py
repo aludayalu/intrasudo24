f5be24667a4dcf6e90549baf575749c48c64e684
aludayalu <pl46u3.ducklinz@gmail.com>
2024-06-23 12:01:26 +0530

    mimetype html

--- mailer.py
+++ mailer.py
@@ -9,12 +9,12 @@ def mail(to, subject, content):
     sender_email = "Exun Clan <exun@dpsrkp.net>"
     sender_password = password
     msg = MIMEMultipart()
-    msg['From'] = sender_email
-    msg['To'] = to
-    msg['Subject'] = subject
-    msg.attach(MIMEText(content, 'plain'))
+    msg["From"] = sender_email
+    msg["To"] = to
+    msg["Subject"] = subject
+    msg.attach(MIMEText(content, "html"))
     try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
         server.login("exun@dpsrkp.net", sender_password)
         text = msg.as_string()
