15bb70de689d41c1614e8b9d0938ca069e05ce40
aludayalu <pl46u3.ducklinz@gmail.com>
2024-06-24 15:20:12 +0530

    proper error logging fix

--- public/chat.js
+++ public/chat.js
@@ -57,7 +57,7 @@ document.getElementById("chatSendButton").addEventListener("click", async (x)=>{
     var text=document.getElementById("chatInput").value.trim().trim("\n")
     if (text!="") {
         document.getElementById("chatInput").value=""
-        var response=await fetch("/submit_message?content="+encodeURIComponent(text))
+        var response=await (await fetch("/submit_message?content="+encodeURIComponent(text))).json()
         if (response.error!=undefined) {
             notyf.error({ position: position, message: response.error })
         } else {
